f7044913b700a3e170e7b40a9c1b0ee63cffe999
TanWeeKiat <tanwee.kiat@live.com.sg>
2019-11-11 21:35:53 +0800

    done with post method

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, :except => [ ]
+
+    def index
+        @post= Post.all
+    end
+  
+    def show
+        @post= Post.find(params[:id])
+    end
+  
+    def new
+    end
+  
+    def edit
+        @post = Post.find(params[:id])
+        
+    end
+  
+    def create
+        @post = Post.new(post_params)
+        
+        @post.user = current_user
+  
+        if @post.save
+      #   render plain: params[:kopi].inspect
+        redirect_to @post
+        else
+          @posts = Post.all
+  
+        
+          render 'new'
+      end
+  end
+
+  
+    def update
+        @post = Post.find(params[:id])
+
+        @post.update(post_params)
+        redirect_to @post
+    end
+  
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+  
+        redirect_to root_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :img_url, :description, :category)
+  end
