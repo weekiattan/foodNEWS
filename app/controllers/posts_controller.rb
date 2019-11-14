f509bcdb65721b51eacc3c73e329b916748506f1
“YOUR-USERNAME” <YOUR-EMAIL-ADDRESS>
2019-11-14 15:26:03 +0800

    added search bar function to search for post, added css to images, added cloudinary for inserting image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
         @categories = Category.all
         @reviews = Review.all
         @user = User.all
+        @post = Post.search(params[:search])
 
     end
 
@@ -30,9 +31,22 @@ class PostsController < ApplicationController
     end
 
     def create
+
+      uploaded_file = params[:post][:cloud].path
+      cloudnary_file = Cloudinary::Uploader.upload(uploaded_file)
+
+      #store this public_id value to the database
+      #cloudnary_file['public_id']
+
+      # render json: cloudnary_file
+      # p cloudnary_file
+      s1 = cloudnary_file["public_id"]
+      s2 = cloudnary_file["url"]
+
         @categories = Category.all
         @post = Post.new(post_params)
-
+        @post.cloud_key = s1
+        @post.cloud_url = s2
         @post.user = current_user
 
         if @post.save
@@ -64,5 +78,5 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :img_url, :description, :category_id, :rating)
+    params.require(:post).permit(:title, :description, :category_id, :rating)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_152505) do
+ActiveRecord::Schema.define(version: 2019_11_14_031213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_11_11_152505) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "category_id"
+    t.text "cloud_key"
+    t.text "cloud_url"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
